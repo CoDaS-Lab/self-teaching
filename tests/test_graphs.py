1ca3d0c177433d8a29e0f53ad3df663f126da8a5
wkvong <waikeenvong@gmail.com>
2018-06-20 12:02:55 -0400

    moved graph active learning examples as test cases

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,8 +1,9 @@
 import pytest
 import numpy as np
+from causal_learning import utils
 from causal_learning.dag import DirectedGraph
-from causal_learning.utils import create_graph_hyp_space
 from causal_learning.graph_teacher import GraphTeacher
+from causal_learning.graph_active_learner import GraphActiveLearner
 
 
 def test_get_parents():
@@ -82,7 +83,7 @@ def test_likelihood():
           0.00802000, 0.00802000, 0.00802000, 0.00802000, 0.64320400, 0.64320400]
          ])
 
-    graphs = create_graph_hyp_space(t=t, b=b)
+    graphs = utils.create_graph_hyp_space(t=t, b=b)
     graph_teacher = GraphTeacher(graphs)
     graph_teacher.likelihood()
 
@@ -103,7 +104,7 @@ def test_causal_graph_simulation():
                             [0.24782723, 0.11684906, 0.05856264],
                             [0.05073297, 0.08789775, 0.02320289]]])
 
-    graphs = create_graph_hyp_space(t=0.8, b=0.01)
+    graphs = utils.create_graph_hyp_space(t=0.8, b=0.01)
     graph_teacher = GraphTeacher(graphs)
     graph_teacher.likelihood()
     graph_teacher.update_teacher_posterior(graph_teacher.learner_prior)
@@ -113,3 +114,63 @@ def test_causal_graph_simulation():
         graph_teacher.teacher_posterior, graph_teacher.sequential_teacher_posterior)
 
     assert np.all(np.isclose(teach_true, teach))
+
+
+def test_graph_active_learner_one():
+    t = 0.8  # transmission rate
+    b = 0.0  # background rate
+
+    # example one
+    common_cause_1 = np.array([[0, 1, 1], [0, 0, 0], [0, 0, 0]])
+    common_cause_1_lik = np.array(
+        [((1-t)*(1-b))**2, (1-t)*(1-b)*(t + (1-t)*b), (t + (1-t)*b)*(1-t)*(1-b), (t + (1-t)*b)**2,
+         (1-b)**2, (1-b)*b, (1-b)**2, (1-b)*b,
+         b*(1-t)*(1-b), b*(t + (1-t)*b), b*(1-t)*(1-b), b*(t + (1-t)*b)])
+
+    common_cause_2 = np.array([[0, 0, 0], [1, 0, 1], [0, 0, 0]])
+    common_cause_2_lik = utils.permute_likelihood(
+        common_cause_1_lik, (2, 1, 3))
+    common_cause_2_lik[7], common_cause_2_lik[10] = \
+        common_cause_2_lik[10], common_cause_2_lik[7]
+
+    graphs_one = [common_cause_2, common_cause_1]
+    likelihoods_one = [common_cause_2_lik, common_cause_1_lik]
+    common_cause_graphs = [DirectedGraph(graph, likelihood, t, b)
+                           for (graph, likelihood) in zip(graphs_one, likelihoods_one)]
+
+    gal_one = GraphActiveLearner(common_cause_graphs)
+    gal_one.update_posterior()
+
+    true_eig = np.array([0.5, 0.5, 0])
+
+    assert np.array_equal(true_eig, gal_one.expected_information_gain())
+
+
+def test_graph_active_learner_two():
+    t = 0.8  # transmission rate
+    b = 0.0  # background rate
+
+    causal_chain_1 = np.array([[0, 1, 0], [0, 0, 1], [0, 0, 0]])
+    causal_chain_1_lik = np.array(
+        [(1-t)*(1-b)*(1-b), (1-t)*(1-b)*b, (t + (1-t)*b)*(1-t)*(1-b), (t + (1-t)*b)**2,
+         (1-b)*(1-t)*(1-b), (1-b)*(t + (1-t)*b), (1-b)**2, (1-b)*b,
+            b*(1 - t)*(1-b), b*(t + (1-t)*b), b*(1-t)*(1-b), b*(t + (1-t)*b)]
+    )
+
+    causal_chain_2 = np.array([[0, 0, 1], [0, 0, 0], [0, 1, 0]])
+    causal_chain_2_lik = utils.permute_likelihood(
+        causal_chain_1_lik, (1, 3, 2))
+    causal_chain_2_lik[1], causal_chain_2_lik[2] = \
+        causal_chain_2_lik[2], causal_chain_2_lik[1]
+
+    graphs_two = [causal_chain_2, causal_chain_1]
+    likelihoods_two = [causal_chain_2_lik, causal_chain_1_lik]
+    causal_chain_graphs = [DirectedGraph(graph, likelihood, t, b)
+                           for (graph, likelihood) in zip(graphs_two, likelihoods_two)]
+
+    gal_two = GraphActiveLearner(causal_chain_graphs)
+    gal_two.update_posterior()
+
+    true_eig = np.array([0.11594429, 0.44202786, 0.44202786])
+
+    assert np.allclose(true_eig, gal_two.expected_information_gain())
