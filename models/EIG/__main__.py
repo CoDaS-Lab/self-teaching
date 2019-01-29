a88cda56551a8000104b11390e23020cb040b0d0
Caroline <guidodarezzo@users.noreply.github.com>
2019-01-29 15:20:39 -0500

    Add files via upload

--- models/EIG/__main__.py
+++ models/EIG/__main__.py
@@ -1,30 +1,28 @@
-import numpy as np
 import os, sys
-
-
 from EIG.eig import Eig
+
 
 def main():
 
+    #* First block handles file i/o exceptions if any *#
     # refactor so fpath is passed through argv[] #
     if not os.path.isfile('non_representative_features.txt'):
         print("file does not exist. exiting...")
         sys.exit(2)
-        
+
+
+    # Instantiate class objects for both representative and non-rep test cases (Markant, "Guess Who") #
     non_rep = Eig('non_representative_features.txt')
     rep = Eig('representative_features.txt')
 
-    for i in range(rep.num_features):
+    # Print IG values for both test cases (sanity check) #
+    for i in range(rep.n_features):
         print(i+ 1, rep.inf_gain(i))
 
     print('*****************************')
 
-    for i in range(non_rep.num_features):
+    for i in range(non_rep.n_features):
         print(i+1, non_rep.inf_gain(i))
-
-
-
-
 
 
 
--- models/EIG/eig.py
+++ models/EIG/eig.py
@@ -4,26 +4,33 @@ import numpy as np
 class Eig:
 
     ## refract notes: call super to init
-    def __init__(self, hyp_space_fpath):
+    def __init__(self, hyp_space_fpath, n_label=2):
 
         self.hyp_space = read_file(hyp_space_fpath)
-        self.num_features = self.hyp_space.shape[1]
-        self.num_hyp = self.hyp_space.shape[0]
+        self.n_features = self.hyp_space.shape[1]
+        self.n_hyp = self.hyp_space.shape[0]
         self.n = self.hyp_space.shape[0]
 
-
+        self.prior = 1 / self.n_hyp * np.ones((self.n_hyp, self.n_features, n_label))
         ## hyp_space: assumes that space is created elsewhere and passed in as file...(maybe refactor)
 
-    def likelihood(self):
+    def prior_ent(self):
 
         return np.log2(self.n)
 
     def update_learner_posterior(self, n_yes, n_no):
 
-        return ((n_no/self.n)*(np.log2(n_no))) + ((n_yes/self.n) * (np.log2(n_yes)))
+        dist = np.sum(self.posterior, axis=0)
+
+        self.posterior = np.nan_to_num(np.divide(self.posterior, dist, where=dist!=0))
+
+    def posterior_ent(self, n_yes, n_no):
+
+        return ((n_no / self.n) * (np.log2(n_no))) + ((n_yes / self.n) * (np.log2(n_yes)))
 
     def entropy(self):
-        pass
+
+        return np.nansum(self.prior * (1 / np.log2(self.prior)), axis=0)
 
     def inf_gain(self, feature_idx):
 
@@ -33,7 +40,7 @@ class Eig:
         if n_yes == 0:
             return 0
 
-        return self.likelihood() - self.update_learner_posterior(n_yes, n_no)
+        return self.prior_ent() - self.posterior_ent(n_yes, n_no)
 
 
 
